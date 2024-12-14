318264ea0bdf479bdffeceda22881d4c0fa829ee
JoaoVictorCRP <joaovictorxl120@gmail.com>
2024-12-13 22:42:04 -0300

    Rendering lists and the importance of 'key' prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
-// Props
-
-// * Read-only properties that are shared between components.
-// * A parent component can send data to a child component.
-// * It works as sending paramethers, but with key and value
-
-// <Component key=value />
-
-import UserGreeting from "./UserGreeting/UserGreeting";
+import List from "./List";
 
 function App() {
   return(
-    <>
-      <UserGreeting isLoggedIn={true} username="JoaoVictor"/>
-      {/* <UserGreeting/> */}
-    </>
+    <List/>
   );
 }
 
