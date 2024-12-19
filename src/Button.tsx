98438341f2344d3bbc51148f3acc0d46ba1c2bc9
JoaoVictorCRP <joaovictorxl120@gmail.com>
2024-12-18 22:19:00 -0300

    Click events

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,21 +1,12 @@
 function Button (){
-    // const handleClick = () => {
-    //     console.log('Ouch, that hurt!')
-    // }
     
-    // const handleClick2 = (name:string) => {console.log(`${name}, stop clicking me!`)}
-
-    let count = 0;
-    let msg = '';
-    const handleClick3 =  (name:string) => {
-        count++;
-        msg = count===1 ? `${name}, you clicked me ${count} time` : msg = `${name}, you cliked me ${count} times`;
-        console.log(msg)
+    const handleClick = (e:MouseEvent<HTMLButtonElement>) => {
+        console.log(e);
+        e.target.textContent = 'Outch! :('; // Modifying button based on the property of the event
     }
 
-
     return (
-        <button onClick={() => handleClick3('Joao')}>Clique aqui :)</button> // Arrow function is used to prevent a callback function to execute imeadiatelly due to its parentesis  
+        <button onClick={(e) => handleClick(e)}>Clique aqui :)</button> // Arrow function is used to prevent a callback function to execute imeadiatelly due to its parentesis  
     );
 }
 
