6e815d444d9587e9d1b07924eda6d11d1236001b
JoaoVictorCRP <joaovictorxl120@gmail.com>
2024-12-26 22:33:54 -0300

    Updating Objects (class) in state

---
+++ src/Car/Car.tsx
@@ -0,0 +1,13 @@
+class Car {
+    manufacturer:string;
+    model:string;
+    year:string;
+
+    constructor(manufacturer:string, model:string, year:string) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.year = year;
+    }
+}
+
+export default Car;
--- src/Container.tsx
+++ src/Container.tsx
@@ -1,48 +1,19 @@
-// update function = A function passed as an argument to setState() usually
-//                   ex: setYear(arrow fucntion)
-//                   Allow for safe updates BASED ON THE PREVIOUS UPDATE
-//                   Used with multiple state updates and asynchronous functions
-//                   TIP: It is a good practice to use update functions whenever possible
+import React, {useState} from "react";
+import Car from "./Car/Car";
 
-
-// Let's update or previous component with an update function
-
-import {useState} from "react";
 function Container() {
-    const [name, setName] = useState<string>("Anônimo");
-    const [age, setAge] = useState<number>(18);
-    const [isEmployed, setEmployed] = useState<boolean>(true);
-    const updateName = () => {
-        const newName = prompt('Insira seu nome:') || name;
-        setName(newName);
-    }
-    const increaseAge = () => {
-        
+    const [car, setCar] = useState<Car>(new Car('Ford', 'Mustang', '2024'))
 
-        setAge(prevAge => prevAge+1); // React documentation recommends to use " prevName " for the previous state, or...
-        setAge(prevAge => prevAge+1);
-        setAge(prevAge => prevAge+1);
-
-        // Update functions are passed as queues to the React library, so it will execute each one of the functions above
-        // in a FIFO scheme. Finally, we will have incremented +3 on the original value.
-    }
-    const decreaseAge = () => {
-        setAge(a => a-1);  // The variable's first letter, in our case, "a". 
-    }
-    const ToggleEmployment = () => {
-        setEmployed(!isEmployed);
-    }
-    return (
+    const handleManufacturerChange = (event:React.ChangeEvent<HTMLInputElement>) => setCar(prevCar => new Car(event.target.value, prevCar.model, prevCar.year));
+    const handleModelChange = (event:React.ChangeEvent<HTMLInputElement>) => setCar(prevCar => new Car(prevCar.manufacturer, event.target.value, prevCar.year));
+    const handleYearChange = (event:React.ChangeEvent<HTMLInputElement>) => setCar(prevCar => new Car(prevCar.manufacturer, prevCar.model, event.target.value));
+    return(
         <div>
-            <p>Nome: {name}</p>
-            <button onClick={updateName}>Alterar nome</button>
-        
-            <p>Age: {age}</p>
-            <button onClick={decreaseAge}>Rejuvenescer -</button>
-            <button onClick={increaseAge}>Envelhecer +</button>
-            
-            <p>Empregado: {isEmployed ? "Sim" : "Não"}</p>
-            <button onClick={ToggleEmployment}>Alterar vínculo</button>
+            <p>Seu carro favorito é: {car.manufacturer} {car.model} {car.year}</p>
+
+            <input type="text" value={car.manufacturer} onChange={handleManufacturerChange}/> <br/>
+            <input type="text" value={car.model} onChange={handleModelChange}/> <br/>
+            <input type="text" value={car.year} onChange={handleYearChange}/> <br/>
         </div>
     );
 }
